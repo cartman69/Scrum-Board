7e094f64143d13dfe3c3703f9eea268649bcec0d
pangolin <amerzone2007>
2017-03-23 15:22:18 +0100

    Projects - Add the "edit" anchor

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -63,7 +63,7 @@ class ProjectController extends Controller
      */
     public function edit(Project $project)
     {
-        //
+        return view('/projects/create', [ 'project' => $project ]);
     }
 
     /**
--- resources/views/projects/list.blade.php
+++ resources/views/projects/list.blade.php
@@ -8,9 +8,10 @@
                 <div class="panel-heading">Create a project</div>
                 <div class="panel-body">
                     @foreach ($projects as $project)
-                        <span style="background-color: #{{ $project->color }}">
-                            <h2 style="background-color: #{{ $project->color }}">{{ $project->name }} ({{ $project->code }})</h2>
-                            <p style="background-color: #{{ $project->color }}">{{ $project->description }}</p>
+                        <span style="background-color:#{{ $project->color }}">
+                            <h2 style="background-color:#{{ $project->color }}">{{ $project->name }} ({{ $project->code }})</h2>
+                            <a href="{{ route('projects.edit', $project) }}">Edit</a>
+                            <p style="background-color:#{{ $project->color }}">{{ $project->description }}</p>
                         </span>
                     @endforeach
                 </div>
